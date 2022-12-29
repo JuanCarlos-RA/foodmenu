af18d84d5842d564b3456b341388a13c2109fc13
Dilan <dilanbu2489@gmail.com>
2022-12-29 17:17:32 -0600

    topBar-1 | border and margins changes

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -1,22 +1,30 @@
 import styled from "styled-components";
 
 const Bar = styled.div` 
-  background-color: black ;
+  background-color: black;
   text-align: center ;
+  margin: 4px;
   padding: 1%;
+
+  border-top-left-radius: 10px ;
+  border-top-right-radius: 10px ;
+  border-bottom-left-radius: 4px;
+  border-bottom-right-radius: 4px;
 `
 
 const Title = styled.h1`
     margin: 0%;
-    color: blue;
+    color: white;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
 `
 
 const TopBar = () => {
     return (
         <Bar>
-            <Title>Cuquis Food</Title>
+            <Title>CUQUI'S FOOD</Title>
         </Bar>
     )
 }
+
 
 export default TopBar
