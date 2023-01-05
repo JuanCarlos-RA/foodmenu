597a787fa2ff919ad7380423a15d9da35575aa67
Dilan <dilanbu2489@gmail.com>
2023-01-04 22:25:09 -0600

    CatFood-0 | add categoryFood

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -1,27 +1,46 @@
 import styled from "styled-components";
 
 const Bar = styled.div` 
-  background-color: black;
-  text-align: center ;
-  margin: 4px;
-  padding: 1%;
+    background-color: transparent;
+    text-align: center ;
+    padding: 1%;
+    box-shadow: rgba(0, 0, 0, 0.80) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
+   
+    background-color: rgba(255, 255, 255, .15);  
+    backdrop-filter: blur(5px);
+    border-bottom-color: transparent ;
 
-  border-top-left-radius: 10px ;
-  border-top-right-radius: 10px ;
-  border-bottom-left-radius: 4px;
-  border-bottom-right-radius: 4px;
+    position:fixed;
+    top:0px; 
+    width:97.9%;
+    
 `
 
 const Title = styled.h1`
+
     margin: 0%;
     color: white;
-    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    text-decoration: underline;
+
 `
+
+// const Design = styled.div`
+//     display: flex;
+//     background-color: burlywood ;
+//     height: 2px;
+//     width: 100px;
+//     position:absolute;  
+// `
 
 const TopBar = () => {
     return (
         <Bar>
-            <Title>CUQUI'S FOOD</Title>
+            
+            <Title>
+                CUQUI'S FOOD
+            </Title>
+            
+
         </Bar>
     )
 }
