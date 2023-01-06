6688cb16d0d7b4edfd804232c8421f3661632f15
Juan Carlos <juancarlosretanaarce@gmail.com>
2023-01-05 21:49:21 -0600

    FM-11 | add colorVariables

--- src/components/categoryFood.js
+++ src/components/categoryFood.js
@@ -1,16 +1,17 @@
 import styled from "styled-components";
+import * as palette from '../utils/colorVariables'
 
 const Categoryboard = styled.div`
     /* box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);  */
-    box-shadow: rgba(163, 87, 9, 0.4) 0px 2px 4px, rgba(163, 87, 9, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
+    box-shadow: ${palette.fifthColor} 0px 2px 4px, ${palette.sixthColor} 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
     margin:4px 4px 20px 4px;
     
     padding-bottom: 1px;
     border-radius: 5% 5% 2% 2%;
-    border: 2px solid #1B1A17;
+    border: 2px solid ${palette.secondColor};
     height: 140px ;
     width: 160px ;
-    background-color: #1B1A17 ;
+    background-color: ${palette.secondColor} ;
     text-align: center;
 `
 const FoodsImg = styled.img`
@@ -21,7 +22,7 @@ const FoodsImg = styled.img`
 `
 
 const Title = styled.h4`
-    color: #FF8303;
+    color: ${palette.thirdColor};
     
 `
 const CategoryFood = ({ cate, url }) => {
---
+++ src/utils/colorVariables.js
@@ -0,0 +1,15 @@
+const firstColor = 'black'
+const secondColor = '#1B1A17'
+const thirdColor = ' #FF8303'
+const fourthColor = '#ffff'
+const fifthColor = 'rgba(163, 87, 9, 0.4)'
+const sixthColor = 'rgba(163, 87, 9, 0.3)'
+
+export {
+    firstColor,
+    secondColor,
+    thirdColor,
+    fourthColor,
+    fifthColor,
+    sixthColor
+}
